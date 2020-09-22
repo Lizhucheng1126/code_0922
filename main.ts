cc21fa4ad8786d667f29838535e188ddbb3e3d19
Lizhucheng1126 <71689249+Lizhucheng1126@users.noreply.github.com>
2020-09-22 15:30:55 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,89 @@
-basic.showNumber(0)
-basic.pause(1000)
-basic.showNumber(1)
-basic.pause(1000)
-basic.showNumber(2)
-basic.pause(1000)
-basic.showNumber(3)
-basic.pause(1000)
-basic.showNumber(4)
 basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
     basic.pause(1000)
-    basic.showString(" 56789")
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . # # #
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # # # .
+        # . # . #
+        `)
 })
