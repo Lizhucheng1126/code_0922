24b72519218458e9ee6a1a9eb14e12bc62d6f170
Lizhucheng1126 <71689249+Lizhucheng1126@users.noreply.github.com>
2020-09-22 15:06:59 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-led.plot(3, 1)
+basic.showNumber(0)
+basic.pause(1000)
+basic.showNumber(1)
+basic.pause(1000)
+basic.showNumber(2)
+basic.pause(1000)
+basic.showNumber(3)
+basic.pause(1000)
+basic.showNumber(4)
 basic.forever(function () {
-	
+    basic.pause(1000)
+    basic.showString(" 56789")
 })
